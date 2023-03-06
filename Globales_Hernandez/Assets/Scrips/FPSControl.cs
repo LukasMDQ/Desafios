2abbcdb17c3b0f931e1c2787a286ab281987c0ab
LukasMDQ <123424547+LukasMDQ@users.noreply.github.com>
2023-03-06 08:55:21 -0300

    Globales

---
+++ Globales_Hernandez/Assets/Scrips/FPSControl.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class FPSControl : MonoBehaviour
+{
+    public float velocidadMovimiento = 3f;
+    //Player Look Rotation
+    public float sensibilidadMouse = 100f;
+    public Transform playerBody;
+    public float xRotacion;
+    public float yRotacion;
+    public float RotationSpeed;
+    public AudioSource _AudSource;
+
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        Move();
+        BloqueoCusor();
+        MouseLook();
+
+    }
+
+
+    void Move()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 inputPlayer = new Vector3(hor, 0, ver);
+        transform.Translate(new Vector3(hor, 0, ver) * velocidadMovimiento * Time.deltaTime);
+        // rb.AddForce(inputPlayer * velocidadMovimiento * Time.deltaTime);
+    }
+   
+    public void BloqueoCusor()//----------------BLOQUEAR CURSOR-----------//
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void MouseLook()
+
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * sensibilidadMouse * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * sensibilidadMouse * Time.deltaTime;
+
+
+
+        xRotacion -= mouseY;
+
+        xRotacion = Mathf.Clamp(xRotacion, -70, 70);
+
+
+
+        yRotacion += mouseX;//esto es asi sino funciona al revez
+
+        //yRotacion = Mathf.Clamp(yRotacion,-360,360);
+
+
+
+        transform.localRotation = Quaternion.Euler(xRotacion, yRotacion, 0);
+
+    }
+}
